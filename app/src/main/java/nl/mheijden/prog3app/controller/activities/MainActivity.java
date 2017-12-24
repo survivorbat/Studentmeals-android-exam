3fdc03b0df62594f2cc837cf2d6fb37d9b2edcb1
AlexBrooklyn <djbatcat@gmail.com>
2017-12-24 12:06:13 +0100

    Initial commit

---
+++ app/src/main/java/nl/mheijden/prog3app/controller/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package nl.mheijden.prog3app.controller.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nl.mheijden.prog3app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
